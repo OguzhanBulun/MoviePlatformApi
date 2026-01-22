8962c6ea32cc08336385c398e43ef4f132fb2f7e
OguzhanBulun <oguzhanbulun24@gmail.com>
2026-01-22 23:38:21 +0300

    Entities and context added

---
+++ MoviePlatformApi.Domain/Entities/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviePlatformApi.Domain.Entities
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public int Rating { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedYear { get; set; }
+        public string MovieName { get; set; }
+        public string Description { get; set; }
+        public string CoverImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ MoviePlatformApi.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviePlatformApi.Domain.Entities;
+
+namespace MoviePlatformApi.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=MoviePlatformDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+    }
+}
